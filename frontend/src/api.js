ce3502da0793be00660429065e67ff190d878fae
Dil Rawat <dbrawat@ysu.edu>
2024-11-03 17:59:43 -0500

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiURL = "/choreo-apis/djangoreactfullstackapp/backend/v1"
+
 // Interceptor: intercept any request we will send and automatically add correct headers
 // so that we don't have to manually add those headers
 // axios to send network requests. 
 // Axios interceptor which will check if we have access token every time we send the request
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 });
 
 api.interceptors.request.use(
